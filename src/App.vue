f2f30eaece85b70b0e6346173e3fbc8cf477dcf8
liumingmusic <liuming_music@163.com>
2017-01-22 20:37:58 +0800

    添加相关的资源信息

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="app">
+    
+  </div>
+</template>
+
+<script>
+export default {
+  data: function(){
+    return {
+
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .app {
+    background-image: url(/static/img/index.db54129.jpg);
+    width: 100%;
+    height: calc(100vh);
+    background-position: center center;
+    background-repeat: no-repeat;
+    background-size: cover;
+  }
+</style>
