38fbff79a12d141bbb648b6f25a6db1cbc833a86
liumingmusic <liuming_music@163.com>
2017-02-06 22:55:56 +0800

    结合muse-ui编写基本的页面，书写布局信息

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -1,14 +1,19 @@
 <template>
-  
+	<div class="app">
+	  
+	</div>
 </template>
 
 <script>
 export default {
-  
+	data: function(){
+		return {
+
+		}
+	}
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 
 </style>
--- src/main.js
+++ src/main.js
@@ -1,19 +1,22 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+//路由和资源操作类库
 import VueRouter from 'vue-router'
 import VueResource from 'vue-resource'
+//muse相关类库
+import MuseUI from 'muse-ui'
+import 'muse-ui/dist/muse-ui.css'
+//使用 carbon 主题
+import 'muse-ui/dist/theme-carbon.css'
+//自定义相关组件
 import App from './App.vue'
-//4、引入bootstrap
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap/dist/js/bootstrap.min'
 
+//组件全部加载使用
+Vue.use(MuseUI)
 Vue.use(VueRouter);
 Vue.use(VueResource);
 
-/* eslint-disable no-new */
 new Vue({
-  el: '#app',
-  template: '<App/>',
-  components: { App }
+    el: '#app',
+    template: '<App/>',
+    components: { App }
 });
